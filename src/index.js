840bf8a700d630c3353acbffdea5a1266a314a21
Iann-rst <iannrst11@gmail.com>
2022-11-26 19:11:08 -0300

    feat: Get Lista com todas as tarefas do user

--- src/index.js
+++ src/index.js
@@ -50,8 +50,10 @@ app.post('/users', (request, response) => {
   response.status(201).json(user);
 });
 
-app.get('/todos', (request, response) => {
-  // Complete aqui
+app.get('/todos', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+
+  return response.status(200).json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
