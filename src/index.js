a1852d21bb6f211325fd38bf3d4dec70a47dec54
Iann-rst <iannrst11@gmail.com>
2022-11-26 20:04:31 -0300

    feat: Create Todo

--- src/index.js
+++ src/index.js
@@ -57,7 +57,20 @@ app.get('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { title, deadline } = request.body;
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
